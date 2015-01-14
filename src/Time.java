c75cc2cce0130519110a483ce26a57fa9b7d1261
Jacob Parnell <jparnell1997@gmail.com>
2015-01-14 09:07:31 -0600

    Added Assignment Files

---
+++ src/Time.java
@@ -0,0 +1,62 @@
+/*
+ * AP CS MOOC
+ * Term 2 - Assignment 1: Time
+ * A class which represents the time of day in hours and seconds.
+ */
+
+public class Time
+{
+     private int hour;
+     private int minute;     
+     
+     /* Complete required constructors and methods here */
+     
+     /*
+      *Default constructor that sets time to 1200.  
+      */ 
+     public Time()
+     {
+     }
+     
+     /*
+      * If h is between 1 and 23 inclusive, set the hour to h. 
+      * Otherwise, set the hour to 0. If m is between 0 and 59 inclusive, 
+      * set the minutes to m. Otherwise, set the minutes to 0. 
+      */ 
+     public Time(int h, int m)
+     {
+       
+     }
+     
+     /* Returns the time as a String of length 4 in the format: 0819. 
+      * Notice that if the hour or minute is one digit, it should 
+      * print a zero first. For example, 6 should print as 06.
+     */
+     public String toString()
+     {
+       
+       return "";
+     }
+     
+     /*
+      * Returns the time as a String converted from military time 
+      * to standard time. For example, 0545 becomes 5:45 am and 
+      * 1306 becomes 1:06 pm.
+      */ 
+     public String convert()
+     {
+       return "";
+     }
+     
+    /*
+     * Advances the time by one minute. 
+     * Remember that 60 minutes = 1 hour. 
+     * Therefore, if your time was 0359, and you add one minute, 
+     * it becomes 0400. 2359 should increment to 0000.
+     */ 
+    public void increment()
+    {
+      
+    }
+      
+}
