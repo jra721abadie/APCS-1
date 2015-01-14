80cd2a0b0ef772edfa0707236f40f0a76b4823fa
Jacob Parnell <jparnell1997@gmail.com>
2015-01-14 08:37:17 -0600

    Added template and Lesson 4 Activities

---
+++ src/Template.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.ArrayList;
+public class Template {
+
+	/**
+	* @param args
+    */
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		
+    }
+}
