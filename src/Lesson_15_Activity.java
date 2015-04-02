ea9b1090f983f3ba4cd869b0213b26e12b3baf04
Jacob Parnell <jparnell1997@gmail.com>
2015-04-02 08:54:57 -0500

    Finished Lesson 15

--- src/Lesson_15_Activity.java
+++ src/Lesson_15_Activity.java
@@ -1,18 +1,36 @@
-import java.util.Scanner;
-import java.lang.Math;
-import java.util.ArrayList;
 public class Lesson_15_Activity {
 
 	/**
 	* @param args
     */
 	
-	public void sortAndPrint(String [] list) {
-		
+	public static void main(String str[])
+	 {
+	   String [] list = { "against" , "forms" , "belief" , "government" , "democratic" , "movement" , "understanding"};
+	   sortAndPrint(list);
+	 }
+	 public static void sortAndPrint(String [] list)
+	 {
+	  for(int n = 0; n < list.length-1; n++)
+	  {
+	    String temp = "z";
+	    int loc = 0;
+	    for(int i = n; i<list.length; i++)
+	    {
+	      if(temp.compareTo(list[i]) > 0)
+	      {
+	        temp = list[i];
+	        loc = i;
+	      }    
+	    }
+	    
+	    String temp2 = list[n];
+	    list[n] = temp;
+	    list[loc] = temp2;
+	  }
+	  for(int i = 0; i <list.length; i++)
+	    System.out.print(list[i] + " ");
+	  
+	  
+	 }
 	}
-	
-	public static void main(String[] args) {
-		Scanner scan = new Scanner (System.in);
-		
-    }
-}
